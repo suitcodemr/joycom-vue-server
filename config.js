e670c07629fdefd5ea166cda77b31450e672dd5f
MirkoRossbach <mirko.rossbach@gmx.de>
2020-02-14 18:45:06 +0100

    fix of port

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	MONGODB:
 		'mongodb+srv://mirko:mirko321@mrcluster-z9bx8.mongodb.net/BA-APP?retryWrites=true&w=majority',
-	PORT: process.env.port || 5000,
+	PORT: process.env.PORT || 5000,
 	SECRET_KEY: 'someverysecretkey'
 };
